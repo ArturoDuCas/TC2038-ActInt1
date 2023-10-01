6543f67543a0ffc2d5121e8e919346e9df23322b
ArturoDuCas <a00833516@tec.mx>
2023-10-01 10:52:03 -0600

    feat: add main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+    Actividad Integradora 1 - TC2038
+    Integrantes:
+        Arturo Duran Castillo - A00833516
+        Andrés Marcelo De Luna Pámanes - A00832239
+    Fecha: 1 de octubre 2023
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+
+    return 0;
+}
